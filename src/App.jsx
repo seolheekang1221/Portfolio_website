1ef6950ddcaac187038c05f7d5f845f7b86a8f5f
seolhee <seolheekang1221@gmail.com>
2022-04-13 09:10:07 -0400

    modify src(2)

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Portfolios from './components/portfolios/Portfolios'
+import Skills from './components/skills/Skills'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Portfolios />
+            <Skills />
+            <Contact />
+            <Footer />
+        </>
+    )
+};
+
+export default App;
---
+++ src/components/portfolios/Portfolios.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Portfolios'
+
+function Portfolios() {
+  return (
+    <div>Portfolios</div>
+  )
+}
+
+export default Portfolios
